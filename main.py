b7e91853116e9b1df67cd88f2b53014dd11a6b27
Eludarf <88209715+Eludarf@users.noreply.github.com>
2025-07-30 17:03:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 import turtle as turtle_module
 import random
 
-# import colorgram
-#
-# rgb_colors = []
-# colors = colorgram.extract('image.jpg', 30)
-#
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_colors.append(new_color)
-#
-# print(rgb_colors)
 turtle_module.colormode(255)
 tim = turtle_module.Turtle()
 tim.speed("fastest")
